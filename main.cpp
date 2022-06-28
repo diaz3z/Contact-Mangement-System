c010f61e6783c91e740012fda6b68cfa7ea8852a
Mohammad Zaid <mdzaid786291@gmail.com>
2022-06-28 12:28:15 +0530

    Defining void start

--- main.cpp
+++ main.cpp
@@ -1,7 +1,31 @@
 #include <iostream>
 #include <Windows.h>
 
+void start();
+int menu();
+int k = 0;
+
+
 int main(){
     std::cout<<"Hellow world";
     return 0;
+}
+
+
+void start()
+{
+    system("color 0B");
+    std::cout<<"\n\n\n\n\n\n\n\n\n";
+    std::cout<<"\t\t\t\t\tLoading ";
+    char x = 219;
+    for(int i = 0; i < 35; i++){
+        std::cout<<x;
+        if(i<10)
+        Sleep(300);
+        if(i>=10 && i<20)
+        Sleep(150);
+        if(i>=10)
+        Sleep(25);
+    }
+    system("cls");
 }
